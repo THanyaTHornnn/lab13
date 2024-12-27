fec9693dcb9e219444256e242a4eedf78fb01913
THanyaTHornnn <woonnasena49@gmail.com>
2024-12-27 17:16:14 +0700

    complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,44 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum = 0, Ari = 0;
+    for(int i = 0 ; i < N ;i++){
+        sum = sum + A[i];
+    }
+     Ari = sum/N;
+     B[0] = Ari;
+    
+    double stand = 0 , sum1 = 0;
+    for(int i = 0 ; i < N ;i++){
+        sum1 = sum1  + pow((A[i] - Ari),2);
+    }
+    stand = sqrt(sum1/N);
+    B[1] = stand;
+
+    double Geomet = 0 ,sum2 = 1;
+    for(int i = 0 ; i < N ;i++){
+        sum2 = sum2 * A[i];
+    }
+    Geomet = pow(sum2,1.0/N);
+    B[2] = Geomet;
+
+    double Harmon = 0, sum3 = 0;
+    for(int i = 0 ; i < N ;i++){
+        sum3 = sum3 + 1/A[i];
+    }
+    Harmon = N/sum3;
+    B[3] = Harmon;
+
+    double min = A[0], max = A[0];
+    for(int i = 0 ; i < N ;i++){
+        if(A[i] > max){
+            max = A[i];
+        }if(A[i] < min){
+            min = A[i];
+        }
+    }
+    B[4] = max;
+    B[5] = min;
+
+}
